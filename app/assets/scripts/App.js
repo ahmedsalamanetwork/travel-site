34f72c6fa5f2817d50e0e4585b846f76473c7c21
salamanetwork <salamanetwork@gmail.com>
2023-10-10 22:32:52 +0400

    Completetd hamburger menu, animated the menu form mobile first

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import '../styles/styles.css';
 
+// JS
+import MobileMenu from './modules/MobileMenu';
+
+// import Person from './modules/Person';
+
 // alert("Hello, from App.js, Congrats!");
 
 
@@ -8,3 +13,18 @@ if(module.hot)
 {
     module.hot.accept();
 }
+
+// class Adult extends Person {
+//     payTaxes() {
+//         console.log(this.name + " now owes zero taxes.");
+//     }
+// }
+
+// let john = new Person("John", "Red");
+// john.greet();
+
+// let jane = new Adult("Jane", "Purple");
+// jane.greet();
+// jane.payTaxes();
+
+let mobileMenu = new MobileMenu();
