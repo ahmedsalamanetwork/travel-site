bb8592104b968a00160d1de935df220f0a2b6a26
salamanetwork <salamanetwork@gmail.com>
2023-10-13 23:09:36 +0400

    Reveal On scroll feature completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,30 +1,12 @@
-import '../styles/styles.css';
+import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
-// JS
-import MobileMenu from './modules/MobileMenu';
-
-// import Person from './modules/Person';
-
-// alert("Hello, from App.js, Congrats!");
-
-
-// Check and Accept of the Hot Module
-if(module.hot)
-{
-    module.hot.accept();
-}
-
-// class Adult extends Person {
-//     payTaxes() {
-//         console.log(this.name + " now owes zero taxes.");
-//     }
-// }
-
-// let john = new Person("John", "Red");
-// john.greet();
-
-// let jane = new Adult("Jane", "Purple");
-// jane.greet();
-// jane.payTaxes();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 let mobileMenu = new MobileMenu();
+
+if (module.hot) {
+    module.hot.accept()
+}
